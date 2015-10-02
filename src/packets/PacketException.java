ed34af0346e628a46d1c13a998ab81ed5a4c8cd5
James Brown <jamesphone180@gmail.com>
2015-10-02 13:47:56 +1300

    Fixed error handling

--- src/packets/PacketException.java
+++ src/packets/PacketException.java
@@ -5,7 +5,7 @@ public class PacketException extends Exception {
 	private static String makeString(int packet, String detail) {
 		InboundPackets packetType = null;
 		
-		if(packet < 0 || packet > InboundPackets.values().length)
+		if(packet >= 0 && packet < InboundPackets.values().length)
 			packetType = InboundPackets.values()[packet];
 		
 		return "Packet error processing " + (packetType == null ? "UNKNOWN" : packetType.name()) + " : " + detail;
